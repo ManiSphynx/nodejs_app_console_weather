d03eb2956179999e0ccc382ceb57b034e49aa6df
emanuel <manii.romeroo@gmail.com>
2022-02-07 19:17:13 -0600

    add axios request and fetch packages

--- app/index.js
+++ app/index.js
@@ -1,9 +1,40 @@
-const { leerInput } = require("../helpers/inquirer");
+const { leerInput, inquirerMenu, pausa } = require("../helpers/inquirer");
+const busquedas = require("../models/busquedas");
 
 const main = async () => {
-  const text = await leerInput("Hola: ");
+  let options = "";
+  const search = busquedas();
 
-  console.log(text);
+  do {
+    options = await inquirerMenu();
+    switch (options) {
+      case 1:
+        // show message
+        const city = await leerInput("Ciudad:");
+        console.log(city);
+
+        // search city
+
+        // select city
+
+        // Clima
+
+        // show results
+        console.log("\nInformacion de la ciudad\n".green);
+        console.log("CIUDAD:");
+        console.log("LATITUD:");
+        console.log("LONGITUD:");
+        console.log("TEMPERATURA:");
+        console.log("TEMPERATURA MINIMA:");
+        console.log("TEMPERATURA MAXIMA:");
+        break;
+
+      default:
+        break;
+    }
+
+    if (options !== 0) await pausa();
+  } while (options !== 0);
 };
 
 main();
---
+++ models/busquedas.js
@@ -0,0 +1,13 @@
+const busquedas = async () => {
+  let historial = [];
+
+  const ciudades = ciudad();
+
+  return ciudades;
+};
+
+const ciudad = async (busqueda = "") => {
+  console.log(busqueda);
+};
+
+module.exports = busquedas;
